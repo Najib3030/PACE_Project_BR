025a02bb4ab0066056f3e6360a3f480c0caf318b
FormMQ <53845037+FormMQ@users.noreply.github.com>
2022-09-26 18:59:33 +1000

    Drag to scroll (WIP)
    Need to finish functionality

--- Main.js
+++ Main.js
@@ -34,7 +34,6 @@ function toggleVuln() {
 
 //Popup variable
 
-
 function togglePopup() {
     document.getElementById("popup").classList.add("active");
     document.getElementById("overlay").classList.add("active");
@@ -45,3 +44,44 @@ function closePopup() {
     document.getElementById("overlay").classList.remove("active");
 }
 
+//Scroll functionality
+const ele = document.getElementById('container');
+ele.scrollTop = 100;
+ele.scrollLeft = 150;
+
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseDownHandler = function (e) {
+    pos = {
+        // The current scroll
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+        
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function () {
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+
+    ele.style.cursor = 'grab';
+    ele.style.removeProperty('user-select');
+};
+
+//https://htmldom.dev/drag-to-scroll/
